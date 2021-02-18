e0901e9c7e07e957f63f05b60f9f1afc7dd25e4c
TurkoBey <78680709+TurkoBey@users.noreply.github.com>
2021-02-18 15:32:32 +0300

    .

---
+++ CSharp-Alan-Hesaplama/CSharp-Alan-Hesaplama/Hesapla.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Alan_Hesaplama
+{
+	class Hesapla
+	{
+        public int UAlani(int UcgenA, int UcgenH)
+        {
+            return (UcgenA * UcgenH) / 2;
+        }
+        public int KAlani(int KareA)
+        {
+            return KareA * KareA;
+        }
+        public int DikAlani(int DikA, int DikH)
+        {
+            return DikA * DikH;
+        }
+        public double DAlani(double r)
+        {
+            return Math.PI * r * r;
+        }
+    }
+}
